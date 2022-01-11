6536f35009248fb93c9e90e00ac468a4d3c6bec0
narirock <nari9rock@hotmail.com>
2022-01-11 20:59:43 -0300

    criando notificação da cofluencia

--- app/Http/Controllers/CofluenceController.php
+++ app/Http/Controllers/CofluenceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Cofluence;
 use App\Models\Signal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class CofluenceController extends Controller
 {
@@ -41,16 +42,24 @@ class CofluenceController extends Controller
                 //nao expirou, atualizar cofluencia
                 $cofluence->counter++;
                 $cofluence->save();
+                if ($cofluence->counter == 5) {
+                    $this->notify($cofluence);
+                }else if($cofluence->counter > 5){
+                    //verificar se apos o sinal deve atualizar o expiry
+                    $cofluence->counter = 1;
+                    $cofluence->save();
+                }
+
             }
         } else {
             //nao existe cofluencia anterior, criar nova cofluencia
-            $this->newCofluence($request);
+            $cofluence = $this->newCofluence($request);
         }
 
         //salvar dados recebidos
         Signal::create($request->all());
 
-        return response()->json(['success' => true]);
+        return response()->json(['success' => true, 'message' => 'Signal received', 'data' => $cofluence->counter ." / 5"]);
     }
 
     public function newCofluence($request)
@@ -62,5 +71,19 @@ class CofluenceController extends Controller
         $cofluence->symbol = $request->symbol;
         $cofluence->expiry = now()->addMinutes($this->getMinutes($request->sinal));
         $cofluence->save();
+
+        return $cofluence;
+    }
+
+    public function notify($cofluence)
+    {
+        $data = array(
+            'sinal' => $cofluence->sinal,
+            'side' => $cofluence->side,
+            'symbol' => $cofluence->symbol,
+            'color' => $cofluence->side == 'sell' ? 'red' : 'green',
+        );
+
+        Redis::publish('-chanel', json_encode($data));
     }
 }
